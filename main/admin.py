82928d490e64f90f86434487429e2132cc14d5f2
Sandeep G L <sandeeplokesh44@gmail.com>
2026-02-19 19:35:26 +0530

    Refactor admin.py to remove duplicate entries
    Removed duplicate imports and registration of models.

--- main/admin.py
+++ main/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Student, Course, Teacher
 
 admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(Teacher)
-=======
-from .models import Course, Student
-
-admin.site.register(Course)
-admin.site.register(Student)
->>>>>>> 44d0765888239cd01da4a5d0a5d9bfe30a5bd489
