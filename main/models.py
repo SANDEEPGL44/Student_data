9945ca61b5593e84061e6cc79f64b043793b13e5
Sandeep G L <sandeeplokesh44@gmail.com>
2026-02-19 19:45:44 +0530

    Fix merge conflicts in models.py
    Removed merge conflict markers and cleaned up the Course model.

--- main/models.py
+++ main/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
 
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
@@ -12,10 +11,6 @@ class Teacher(models.Model):
 class Course(models.Model):
     title = models.CharField(max_length=100)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-=======
-class Course(models.Model):
-    title = models.CharField(max_length=100)
->>>>>>> 44d0765888239cd01da4a5d0a5d9bfe30a5bd489
 
     def __str__(self):
         return self.title
@@ -26,8 +21,5 @@ class Student(models.Model):
     age = models.IntegerField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
-<<<<<<< HEAD
-=======
     def __str__(self):
         return self.name
->>>>>>> 44d0765888239cd01da4a5d0a5d9bfe30a5bd489
